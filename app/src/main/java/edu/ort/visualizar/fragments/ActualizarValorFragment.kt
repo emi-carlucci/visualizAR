5ef79fcc0307b93f40392fcf512215b8d104ba1c
shogakukan <shogakukan@gmail.com>
2021-05-17 21:41:58 -0300

    navegacion desde acciones

--- app/src/main/java/edu/ort/visualizar/fragments/ActualizarValorFragment.kt
+++ app/src/main/java/edu/ort/visualizar/fragments/ActualizarValorFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import edu.ort.visualizar.R
+import edu.ort.visualizar.models.KpiModel
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -19,15 +20,17 @@ private const val ARG_PARAM2 = "param2"
  */
 class ActualizarValorFragment : Fragment() {
     // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private var indicador: KpiModel? = null
+    //private var param2: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+            //param1 = it.getString(ARG_PARAM1)
+            //param2 = it.getString(ARG_PARAM2)
         }
+        indicador = ActualizarValorFragmentArgs.fromBundle(requireArguments()).indicador
+
     }
 
     override fun onCreateView(
