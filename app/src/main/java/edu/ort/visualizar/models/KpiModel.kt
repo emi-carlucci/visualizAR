0953a134e471708c48f891a59b0b98de41e969c5
emmicarlucci <emiliano.carlucci@uala.com.ar>
2021-05-31 10:46:07 -0300

    serializable exception fixed

--- app/src/main/java/edu/ort/visualizar/models/KpiModel.kt
+++ app/src/main/java/edu/ort/visualizar/models/KpiModel.kt
@@ -31,108 +31,108 @@ data class KpiModel (
 
 data class Category (
         var value: List<String>?
-)
+): Serializable
 
 data class DateModified (
         var type: String?,
         var value: String?
-)
+): Serializable
 
 data class CalculationFrequency (
         var value: String?
-)
+): Serializable
 
 data class Description (
         var value: String?
-)
+): Serializable
 
 data class CurrentStanding (
         var value: String?
-)
+): Serializable
 
 data class Address (
         var type: String?,
         var value: AddressValue?
-)
+): Serializable
 
 data class AddressValue (
         var addressLocality: String?,
         var addressCountry: String?
-)
+): Serializable
 
 data class CalculationPeriod (
         var value: CalculationPeriodValue?
-)
+): Serializable
 
 data class CalculationPeriodValue (
         var to: String?,
         var from: String?
-)
+): Serializable
 
 data class DateNextCalculation (
         var type: String?,
         var value: String?
-)
+): Serializable
 
 data class CalculationMethod (
         var value: String?
-)
+): Serializable
 
 data class Provider (
         var value: ProviderValue?
-)
+): Serializable
 
 data class ProviderValue (
         var name: String?
-)
+): Serializable
 
 data class Organization (
         var value: OrganizationValue?
-)
+): Serializable
 
 data class OrganizationValue (
         var name: String?
-)
+): Serializable
 
 data class KpiValue (
         var value: String?
-)
+): Serializable
 
 data class Name (
         var value: String?
-)
+): Serializable
 
 data class Source (
         var value: String?
-)
+): Serializable
 
 data class Process (
         var value: String?
-)
+): Serializable
 
 data class BusinessTarget (
         var value: String?
-)
+): Serializable
 
 data class CalculationFormula (
         var value: String?
-)
+): Serializable
 
 data class DateCreated (
         var value: String?
-)
+): Serializable
 
 data class DateExpires (
         var value: String?
-)
+): Serializable
 
 data class UpdatedAt (
         var value: String?
-)
+): Serializable
 
 data class Area (
         var value: String?
-)
+): Serializable
 
 
 
